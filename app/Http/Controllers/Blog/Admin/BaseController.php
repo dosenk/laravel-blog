7ebd500d9fc64da2a4d9e4bb796c609bf9748de8
dosenk <eympel@gmail.com>
2019-11-06 18:22:36 +0300

    first commit /u00014

---
+++ app/Http/Controllers/Blog/Admin/BaseController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Blog\admin;
+
+use App\Http\Controllers\Blog\BaseController as GuestBaseController;
+
+abstract class BaseController extends GuestBaseController
+{
+    //
+}
